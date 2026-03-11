d7bb11b56d72efb24178aa5db536f154bfb5ee83
spaceh3ad <jan.kwiatkowski.web3@gmail.com>
2026-03-11 12:35:04 +0100

    Make createPool virtual for inheritance

--- contracts/UniswapV3Factory.sol
+++ contracts/UniswapV3Factory.sol
@@ -36,7 +36,7 @@ contract UniswapV3Factory is IUniswapV3Factory, UniswapV3PoolDeployer, NoDelegat
         address tokenA,
         address tokenB,
         uint24 fee
-    ) external override noDelegateCall returns (address pool) {
+    ) external virtual override noDelegateCall returns (address pool) {
         require(tokenA != tokenB);
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0));
